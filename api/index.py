12c6e4d4c326d18a3aca8fdcef5b84c29e955073
Matheus Rizzati <matheustassinaririzzati@gmail.com>
2024-05-16 16:19:39 -0300

    excel run

--- api/index.py
+++ api/index.py
@@ -1,12 +1,19 @@
 from flask import Flask, jsonify, request
 import pandas as pd
+import os
 from flask_cors import CORS
 
 app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print(BASE_DIR)
+excel_file_path = os.path.join(BASE_DIR, 'tipi.xlsx')
+
 CORS(app)
+
 @app.route('/')
 def search():
-    df = pd.read_excel('./tipi.xlsx')
+    df = pd.read_excel(excel_file_path)
     df = df.dropna(subset=['ALIQUOTA'])
 
     ncm = request.args.get('ncm')
