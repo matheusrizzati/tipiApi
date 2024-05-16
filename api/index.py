11770a2f335f7f4bfd4a4053c492256785365c29
Matheus Rizzati <matheustassinaririzzati@gmail.com>
2024-05-16 15:32:18 -0300

    first commit

---
+++ api/index.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/')
+def search():
+    df = pd.read_excel('C:/Users/mathe/Desktop/Lodes/api-tipi/api/tipi.xlsx')
+    df = df.dropna(subset=['ALIQUOTA'])
+
+    ncm = request.args.get('ncm')
+    
+    if ncm:
+        result = df.loc[df['NCM'] == ncm].to_dict(orient='records')
+    else:
+        result = df.to_dict(orient='records')
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
